4819bb09e39ad5ab442f7542ed82bc0f5f3f9ab7
Amos-17-prog <tharuamos012@gmail.com>
2025-12-07 15:37:52 +0000

    converison of DataTypes

--- Basics/conversion.js
+++ Basics/conversion.js
@@ -22,4 +22,43 @@ let isLoggedIn = "Amos"
 
 let someNum = 33
 let stringNumber = String(someNum)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+
+// ******** Operations *********
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2));
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Amos"
+
+let Str3 = str1 + str2;
+// console.log(Str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + 2);
+
+// console.log(3 + 4 * 5 % 2); // Don't write like this 
+
+// console.log(+true);
+// console.log(+ "");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
