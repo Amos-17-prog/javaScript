03d4ebda0cc002f28da5f612094c38d20e5117aa
Amos-17-prog <tharuamos012@gmail.com>
2025-12-07 03:19:51 +0000

    conversion of DataTypes

---
+++ Basics/conversion.js
@@ -0,0 +1,25 @@
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumver = Number(score)
+// console.log(typeof valueInNumver)
+// console.log(valueInNumver)
+
+
+// "33" = 33
+// "33abc" = NaN
+// true => 1 / false => 0
+
+let isLoggedIn = "Amos"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true / 0 => false
+// "" => false
+// "Amos" => true
+
+let someNum = 33
+let stringNumber = String(someNum)
+console.log(typeof stringNumber)
