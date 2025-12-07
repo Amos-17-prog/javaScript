2f4ddc0277b3f9b03931e27f6e738b57a803d157
Amos-17-prog <tharuamos012@gmail.com>
2025-12-07 02:52:53 +0000

    dataTypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js Code as newer version
+
+// alert(3 + 3) // we're using nodejs, not browser
+
+// console.log(3
+// - 1); // This break the code readability
+
+console.log("Amos"); 
+
+let name = "Amos"
+let age = 22
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power of 53
+// bigint
+// string => ""
+// bool => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object => 
+
+console.log(typeof null) // Null known as objects 
+console.log(typeof undefined) // known as undefined
