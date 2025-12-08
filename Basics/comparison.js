441a923024db95c41515e50bc0090a072dff1d96
Amos-17-prog <tharuamos012@gmail.com>
2025-12-08 11:36:57 +0000

    comparison

---
+++ Basics/comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 * 1);
+// console.log(2 ** 2);
+// console.log(2==2);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// Prefer to avoid this types of conversion 
+// Because it's confusing to understand
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === -> This check the data types also
+console.log("2" === 2); 
