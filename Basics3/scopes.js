8ee82d4780bbd6a11c437d2f26c0d3e56db31cdf
Amos Tharu <tharuamos012@gmail.com>
2025-12-21 13:38:29 +0000

    Scopes and Hoisting

--- Basics3/scopes.js
+++ Basics3/scopes.js
@@ -5,17 +5,62 @@ if (true) { // All the variable use in {} only but var updated anywhere
     let a = 10;
     const b = 20;
     // c = 30
-    console.log("Inner a: " +  a);
+    // console.log("Inner a: " +  a);
     
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
 // Checking scope in Browser and code environment using node are different 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// **** Nested Scope ****
+function one(){
+    const username = "Amos";
+
+    function two(){
+        const website = "YouTube"
+        // console.log(username);
+    }
+    // console.log(website);
+
+    // two() 
+    
+}
+
+
+one()
+
+if(true){
+    const username = "Coder"
+    if(username === "Coder"){
+        const website = "YouTube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+// ********* Interesting ***********
+
+console.log(addone(5)) // We can access this type of function before initialize
+function addone(num) {
+    return num + 1
+}
+
+
+addTwo(5) // We cannot access before initialization because it holds the value in addTwo
+
+const addTwo = function(num){
+    return num + 2
+}
