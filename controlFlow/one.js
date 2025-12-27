4130ca4cd868f32fe643411783da3bbb5b682373
Amos Tharu <tharuamos012@gmail.com>
2025-12-27 14:07:09 +0000

    Control Flow

---
+++ controlFlow/one.js
@@ -0,0 +1,17 @@
+// This basic files
+// Control flow refers if loggedIn and execute some code if loggedOut then execute other coder
+
+// if (2 === "2") { // condition is true then execute if's code
+//     console.log("2 is equal to 2");
+// } else{
+//     console.log("2 is not equal to \"2\"");
+// }
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+// <, >, <=, >=, ==, ===, !=, !==
+
+// Logical operator
