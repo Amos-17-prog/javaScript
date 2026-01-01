3f16b4b47fb4a8bc874f06081dd9bdb916b84f8c
Amos Tharu <tharuamos012@gmail.com>
2026-01-01 16:00:51 +0000

    Map and Reduce

---
+++ Iterations/mapAndReduce.js
@@ -0,0 +1,36 @@
+// const coding = ["Python", "CPP", "JS", "Java", "Ruby", "PHP"]
+
+// const values = coding.forEach((item) => {
+//     console.log(item); 
+// })
+
+// const values = coding.forEach((item) => {
+//     console.log(item); 
+//     return item
+// })
+
+// console.log(values); // undefined, because forEach doesnot return any value 
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// In filter, there is a callback fn which access each value, but we have to give the ccondition
+// const retNum = myNums.filter((num) => num > 5) // greater numbers than 5
+
+// const retNum = myNums.filter((num) => {
+//      return num > 5
+// }) // if we write scope then we've to write return keywords
+
+// console.log(retNum);
+
+// const newNums = []
+
+// myNums.forEach((num) => { 
+//     if (num>5) {
+//         newNums.push(num)
+//     }
+// }) // works as same as filter
+
+// console.log(newNums);
+
+
+
