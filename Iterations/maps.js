f6a375651d561a0bde616ab177ebe9ca210a66ee
Amos Tharu <tharuamos012@gmail.com>
2026-01-02 14:39:42 +0000

    Reduce

---
+++ Iterations/maps.js
@@ -0,0 +1,11 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.map((num) => num + 10)
+
+
+// Chaining means methods after methods
+const newNums = myNums
+    .map((num) => num * 10)
+    .map((num) => num + 1) // This doesnot return the value as array, but updated the value what first method call
+    .filter((num) => num >= 40)
+console.log(newNums);
